dc20f4127bfecb0e6eb519af92370bc272563572
xaviermontane <xaviermontane3@gmail.com>
2024-07-07 20:15:45 +0200

    modified:   bootstrap.sh 	modified:   shell/.bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,12 @@
 # Setup for sec configs, might expand into a singular setup script for all dotfiles
 
 chmod 600 ~/.ssh/config # readable and writable only by the user
+chmod 600 ~/.ssh/id_rsa # readable and writable only by the user
+chmod 644 ~/.ssh/id_rsa.pub # readable by everyone, writable only by the user
+chmod 644 ~/.ssh/known_hosts # readable by everyone, writable only by the user
+chmod 644 ~/.ssh/authorized_keys # readable by everyone, writable only by the user
+
+# --- --- ---
+
+# Setup for bashrc
+cp ./shell/.bashrc ~/.bashrc
